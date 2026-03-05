010082375282f98060b71e297e2dccc3a27873f1
Jy'Mir Fuller <jyjyfuller@gmail.com>
2026-03-04 20:26:03 -0500

    added a ratio route and a loan interest route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import financeRoutes from './routes/finance.js';
 
 dotenv.config();
 
@@ -13,6 +14,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Backend is working !');
 });
+
+app.use('/api/finance', financeRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
